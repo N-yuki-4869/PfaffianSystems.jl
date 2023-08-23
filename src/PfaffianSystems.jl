1b4179d11f89bbde774b00442415ed54241021a7
N-yuki-4869 <u790180d@ecs.osaka-u.ac.jp>
2023-08-23 17:41:23 +0900

    export normalform

--- src/PfaffianSystems.jl
+++ src/PfaffianSystems.jl
@@ -53,7 +53,7 @@ include("WeylAlgebra.jl")
 export weyl_algebra, coerce, elem_type
 
 include("DiffOpRings.jl")
-export diff_op_ring, coerce, elem_type
+export diff_op_ring, coerce, elem_type, normalform
 
 include("DIdeals.jl")
 export DIdeal, intersection_DIdeal, stdmon
